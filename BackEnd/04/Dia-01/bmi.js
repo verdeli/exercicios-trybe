fbf99833b44324de9886f69a1f9bd209aa391a97
Jonathan Verdeli Costa <jonathanverdeli@gmail.com>
2022-12-11 11:24:12 -0300

    feat exercicio1 consumindo menos RAM e processador

--- BackEnd/04/Dia-01/bmi.js
+++ BackEnd/04/Dia-01/bmi.js
@@ -1,55 +1,106 @@
 //Exercicio 01
 
-function calculateIMC (){
-const readline = require('readline-sync');
+// function calculateIMC (){
+// const readline = require('readline-sync');
 
-const weight = readline.question('Qual o seu peso: ');
-const height = readline.question('Qual a sua altura: ');
+// const weight = readline.question('Qual o seu peso: ');
+// const height = readline.question('Qual a sua altura: ');
 
-const imc = (weight / (height*height)).toFixed(2);
-console.log(imc); 
+// const imc = (weight / (height*height)).toFixed(2);
+// console.log(imc); 
 
-const MAGREZA = 18.5;
-const NORMAL = 18.5;
-const SOBREPESO = 25;
-const OBESIDADE1 = 35;
-const OBESIDADE2 = 40;
+// const MAGREZA = 18.5;
+// const NORMAL = 18.5;
+// const SOBREPESO = 25;
+// const OBESIDADE1 = 35;
+// const OBESIDADE2 = 40;
 
-switch (true) {
-  case imc > OBESIDADE2:
-    console.log('Obesidade grau III e IV, muito gordo, necessita emagrecer');
+// switch (true) {
+//   case imc > OBESIDADE2:
+//     console.log('Obesidade grau III e IV, muito gordo, necessita emagrecer');
 
-    break;
-  case imc > OBESIDADE1:
-    console.log('Obesidade grau II, quantidade de gordura acima do normal, necessita emagrecer');
+//     break;
+//   case imc > OBESIDADE1:
+//     console.log('Obesidade grau II, quantidade de gordura acima do normal, necessita emagrecer');
     
-    break;
+//     break;
 
-  case imc > SOBREPESO:
-    console.log('Obesidade grau I, tá mei gordin, mais dieta e academia');
+//   case imc > SOBREPESO:
+//     console.log('Obesidade grau I, tá mei gordin, mais dieta e academia');
 
-    break;
+//     break;
   
-  case imc > NORMAL:
-    console.log('peso Normal');
+//   case imc > NORMAL:
+//     console.log('peso Normal');
 
-    break;
+//     break;
 
-  case imc < MAGREZA:
-    console.log('Peso abaixo do normal, magreza');
+//   case imc < MAGREZA:
+//     console.log('Peso abaixo do normal, magreza');
     
-    break;
+//     break;
 
-  default:
-    console.log('Programa entrou aonde não devia!');
-    break;
+//   default:
+//     console.log('Programa entrou aonde não devia!');
+//     break;
+// }
+// }
+// calculateIMC();
+
+
+//exercicio 1 de uma outra maneira
+
+const IMC = {
+  'Abaixo do peso(magreza)':{
+    min: 0,
+    max: 18.4,
+  },
+  'Peso normal': {
+    min: 18.5,
+    max: 24.9,
+  }, 
+  'Acima do peso (sobrepeso)': {
+    min: 25,
+    max: 29.9,
+  },
+  'Obesidade grau I': {
+    min: 30,
+    max: 34.9,
+  },
+  'Obesidade grau II': {
+    min: 35,
+    max: 39.9,
+  },
+  'Obesidade grau III e IV': {
+    min: 40,
+    max: 600,
+  },
 }
+
+function calculaIMC(){
+  
+  const readline = require('readline-sync');
+  
+  const weight = readline.question('Qual o seu peso: ');
+  const height = readline.question('Qual a sua altura: ');
+
+  const imc = (weight / (height*height)).toFixed(2);
+
+  const statusIMC = Object.keys(IMC);
+  
+  const statusUser = statusIMC.find((status) =>{
+    
+    const {min, max} = IMC[status]; 
+    
+    return imc >= min && imc <= max;
+    
+  });
+  
+  console.log(statusUser);
+  
 }
-calculateIMC();
 
-
-//exercicio 2
-
+calculaIMC();
 
 
 
